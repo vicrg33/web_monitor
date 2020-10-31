fc352448ef0ad06d9644e01bcb52cef8b7cd3bd1
VicWinWork <vic.vrg93@gmail.com>
2020-10-31 14:28:16 +0100

    Color fixes

--- main.py
+++ main.py
@@ -197,13 +197,9 @@ def format_differences(old, new, link):
                           content="text/html; charset=utf-8" />
                     <style type="text/css">
                         .diff-insert{background-color:#82F67C}
-                        .diff-del{background-color:#FF4A4A}
+                        .diff-del{background-color:#FF6666}
                         .diff-insert-formatting{background-color:#82F67C}
-                        .diff-delete-formatting{background-color:#FF4A4A}
-                        .mark-diff-insert{background-color:#82F67C}
-                        .mark-diff-delete{background-color:#FF4A4A}
-                        .mark-diff-insert-formatting{background-color:#82F67C}
-                        .mark-diff-delete-formatting{background-color:#FF4A4A}
+                        .diff-delete-formatting{background-color:#FF6666}
                     </style>
                 </head>
                 <body>
