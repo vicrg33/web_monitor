a62d363c5d59c44cd2d20580ce3a5a46938f8262
VicWork <vic1993victor1993@gmail.com>
2021-12-16 00:45:09 +0100

    Final version - v1.3.1 - Minor fixes

--- main.py
+++ main.py
@@ -77,8 +77,6 @@ def check_status(path, name):
                 options.add_experimental_option('excludeSwitches', ['enable-logging'])
                 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
                 options.add_argument('user-agent={0}'.format(user_agent))
-                user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-                options.add_argument('user-agent={0}'.format(user_agent))
                 options.add_argument("--disable-gpu")
                 if website["login_needed"]:
                     if not os.path.exists(path + '\\chrome_tmp\\' + website["name"]):
