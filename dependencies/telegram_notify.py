ba3ec9b350bcdcf7ebdcca4230c421aa9e946614
VicWork <vic1993victor1993@gmail.com>
2022-02-17 12:01:28 +0100

    Final version - v1.4.2 - Minor fixes

--- dependencies/telegram_notify.py
+++ dependencies/telegram_notify.py
@@ -1,10 +1,17 @@
 import requests
 import json
+import platform
 
 
 def telegram_notify(text):
+    # Setting path for Windows and Mac
+    if platform.system() == 'Windows':
+        path = 'D:/OneDrive - gib.tel.uva.es/Personal/Scripts Utiles/web_monitor'
+    elif platform.system() == 'Darwin':
+        path = '/Users/Vic/OneDrive - gib.tel.uva.es/Personal/Scripts Utiles/web_monitor'
+
     # Load json with the sensitive data
-    json_info = open('../bot_data.json')
+    json_info = open(path + '/bot_data.json')
     bot_data = json.load(json_info)
     json_info.close()
     bot_token = bot_data["bot_token"]  # Bot token
